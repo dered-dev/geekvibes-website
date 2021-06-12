c53e5b4e4eea74a673ef68fe0cf72c4320d2045d
dered-dev <jlcc170@gmail.com>
2021-06-11 21:32:36 -0500

    ADD Final text

--- assets/js/base.js
+++ assets/js/base.js
@@ -81,3 +81,25 @@ function clickHandler(e) {
   });
 
 }
+
+
+// send form
+
+const request = new XMLHttpRequest()
+request.open('POST','https://python2g-default-rtdb.firebaseio.com/koders/.json')
+request.addEventListener('readystatechange', () => {
+    if(request.readyState !== 4) {
+        return 
+    } else {
+        if(request.status >= 200 && request.status <= 299){
+            const response  = request
+            const objectResponse = JSON.parse(response.responseText)
+            console.log(objectResponse)
+        } else {
+            console.log('No se pudo ejecutar')
+        }
+    } 
+})
+request.send(
+    JSON.stringify(objNewKoder)
+)
